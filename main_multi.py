d9e431262d2b0e7530519f8f9d3bc35699bad6bd
Womsxd <45663319+Womsxd@users.noreply.github.com>
2021-05-30 12:27:16 +0800

    main_multi.py Ctrl+C后不会报错

--- main_multi.py
+++ main_multi.py
@@ -25,7 +25,11 @@ def main_multi():
         tools.log.info(f"已搜索到{len(config_List)}个配置文件，正在开始执行！")
     else:
         tools.log.info(f"已搜索到{len(config_List)}个配置文件，请确认是否无多余文件！\r\n{config_List}")
-        input("请输入回车继续，需要重新搜索配置文件请Ctrl+C退出脚本")
+        try:
+            input("请输入回车继续，需要重新搜索配置文件请Ctrl+C退出脚本")
+            exit()
+        except:
+            exit()
     for i in iter(config_List):
         tools.log.info(f"正在执行{i}")
         setting.mihoyobbs_List_Use = []
@@ -36,4 +40,5 @@ def main_multi():
 
 if __name__ == "__main__":
     main_multi()
+    sys.exit(0)
 pass
