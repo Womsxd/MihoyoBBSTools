1bd0248625389b8066b323852f86cbafa793df8b
Womsxd <45663319+Womsxd@users.noreply.github.com>
2021-12-02 10:53:30 +0800

    可忽略米游币相关功能

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def main():
     if config.enable_Config:
         # 检测参数是否齐全，如果缺少就进行登入操作
         if config.mihoyobbs_Login_ticket == "" or config.mihoyobbs_Stuid == "" or config.mihoyobbs_Stoken == "":
-            # 登入
-            login.login()
+            # 登入，如果没开启bbs全局没打开就无需进行登入操作
+            if config.mihoyobbs["bbs_Global"]:
+                login.login()
             time.sleep(random.randint(2, 8))
         # 获取要使用的BBS列表,#判断是否开启bbs_Signin_multi
         if config.mihoyobbs["bbs_Signin_multi"]:
