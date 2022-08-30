a6d67d3842cf72734e40fc49fc192a3a79c3d649
Womsxd <45663319+Womsxd@users.noreply.github.com>
2022-08-30 09:47:35 +0800

    429 Too Many Requests 尝试缓解

--- genshin.py
+++ genshin.py
@@ -18,7 +18,8 @@ class Genshin:
                                   f'&act_id={setting.genshin_Act_id}&utm_source=bbs&utm_medium=mys&utm_campaign=icon'
         self.headers['Cookie'] = config.config["account"]["cookie"]
         self.headers['x-rpc-device_id'] = tools.get_device_id()
-        self.headers['User-Agent'] = f'{config.config["games"]["cn"]["useragent"]} miHoYoBBS/{setting.mihoyobbs_Version}'
+        self.headers[
+            'User-Agent'] = f'{config.config["games"]["cn"]["useragent"]} miHoYoBBS/{setting.mihoyobbs_Version}'
         self.account_list = get_account_list("hk4e_cn", self.headers)
         if len(self.account_list) != 0:
             self.checkin_rewards = self.get_checkin_rewards()
@@ -47,12 +48,17 @@ class Genshin:
 
     def check_in(self, account):
         for i in range(4):
+            if i != 0:
+                log.info(f'触发验证码，即将进行第{i}次重试，最多3次')
             req = http.post(url=setting.genshin_Signurl, headers=self.headers,
                             json={'act_id': setting.genshin_Act_id, 'region': account[2], 'uid': account[1]})
+            if req.status_code == 429:
+                time.sleep(10)  # 429同ip请求次数过多，尝试sleep10s进行解决
+                log.warning(f'429 Too Many Requests ，即将进入下一次请求')
+                continue
             data = req.json()
             if data["retcode"] == 0 and data["data"]["success"] == 1:
-                log.info(f'触发验证码，即将进行第{i+1}次重试，最多3次')
-                time.sleep(random.randint(4, 10))
+                time.sleep(random.randint(6, 15))
             else:
                 break
         return req
@@ -78,19 +84,24 @@ class Genshin:
                     else:
                         time.sleep(random.randint(2, 8))
                         req = self.check_in(i)
-                        data = req.json()
-                        if data["retcode"] == 0 and data["data"]["success"] == 0:
-                            log.info(f"旅行者{i[0]}签到成功~\r\n今天获得的奖励是"
-                                     f"{tools.get_item(self.checkin_rewards[0 if sign_days == 0 else sign_days + 1])}")
-                            sign_days += 2
-                        elif data["retcode"] == -5003:
-                            log.info(f"旅行者{i[0]}今天已经签到过了~\r\n今天获得的奖励是{tools.get_item(self.checkin_rewards[sign_days])}")
+                        if req.status_code != 429:
+                            data = req.json()
+                            if data["retcode"] == 0 and data["data"]["success"] == 0:
+                                log.info(f"旅行者{i[0]}签到成功~\r\n今天获得的奖励是"
+                                         f"{tools.get_item(self.checkin_rewards[0 if sign_days == 0 else sign_days + 1])}")
+                                sign_days += 2
+                            elif data["retcode"] == -5003:
+                                log.info(
+                                    f"旅行者{i[0]}今天已经签到过了~\r\n今天获得的奖励是{tools.get_item(self.checkin_rewards[sign_days])}")
+                            else:
+                                s = "账号签到失败！"
+                                if data["data"] != "" and data.get("data").get("success", -1):
+                                    s += "原因: 验证码\njson信息:" + req.text
+                                log.warning(s)
+                                ok = False
                         else:
-                            s = "账号签到失败！"
-                            if data["data"] != "" and data.get("data").get("success", -1):
-                                s += "原因: 验证码\njson信息:" + req.text
-                            log.warning(s)
                             ok = False
+                            data = {"data": {"msg": "429 Too Many Requests"}}
                     if ok:
                         return_data += f"\n{i[0]}已连续签到{sign_days}天\n" \
                                        f"今天获得的奖励是{tools.get_item(self.checkin_rewards[sign_days - 1])}"
