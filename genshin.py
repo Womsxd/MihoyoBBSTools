5cd3531659423c62b899820cb708d17c51bfc676
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-04-23 07:45:59 +0800

    添加返回类型

--- genshin.py
+++ genshin.py
@@ -3,7 +3,7 @@ from gamecheckin import GameCheckin
 
 
 class Genshin(GameCheckin):
-    def __init__(self):
+    def __init__(self) -> None:
         super(Genshin, self).__init__("hk4e_cn", setting.genshin_checkin_rewards)
         self.headers['Referer'] = f'https://webstatic.mihoyo.com/bbs/event/signin-ys/index.html?bbs_auth_required=tru' \
                                   f'e&act_id={setting.genshin_Act_id}&utm_source=bbs&utm_medium=mys&utm_campaign=icon'
