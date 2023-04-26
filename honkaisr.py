f32f61435d04ed8be6060f9ceeb0b5b765e1f8d6
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-04-26 11:00:09 +0800

    fix

--- honkaisr.py
+++ honkaisr.py
@@ -9,6 +9,6 @@ class Honkaisr(GameCheckin):
                                   f'act_id={setting.honkai_sr_Act_id}&bbs_auth_required=true&bbs_presentation_style' \
                                   f'=fullscreen&utm_source=share&utm_medium=bbs&utm_campaign=app'
         self.act_id = setting.honkai_sr_Act_id
-        self.game_mid = "honkai——sr"
+        self.game_mid = "honkai_sr"
         self.game_name = "崩坏: 星穹铁道"
         self.player_name = "开拓者"
