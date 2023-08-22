89f733e90d6f6ecbbbc4e63814bc80a535f708d3
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-08-22 20:58:09 +0800

    Update hoyo_gs.py #70

--- hoyo_gs.py
+++ hoyo_gs.py
@@ -5,7 +5,7 @@ from hoyo_checkin import hoyo_checkin
 def run():
     cookie_str = config.config.get("games", {}).get(
         "os",  {}).get("cookie", "")
-    ret_msg = '原神:\n' + hoyo_checkin("https://hk4e-api-os.mihoyo.com/event/sol",
+    ret_msg = '原神:\n' + hoyo_checkin("https:/sg-hk4e-api.hoyolab.com/event/sol",
                                      "e202102251931481",
                                      cookie_str,)
     return ret_msg
