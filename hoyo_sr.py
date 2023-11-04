95c424b631517241729a25d3663994fcbc0b7688
Womsxd <45663319+Womsxd@users.noreply.github.com>
2023-11-04 18:10:28 +0800

    格式化
    Signed-off-by: Womsxd <45663319+Womsxd@users.noreply.github.com>

--- hoyo_sr.py
+++ hoyo_sr.py
@@ -4,8 +4,7 @@ from hoyo_checkin import hoyo_checkin
 
 def run():
     cookie_str = config.config.get("games", {}).get(
-        "os",  {}).get("cookie", "")
+        "os", {}).get("cookie", "")
     ret_msg = '崩坏:星穹铁道:\n' + hoyo_checkin("https://sg-public-api.hoyolab.com/event/luna/os",
-                                     "e202303301540311",
-                                     cookie_str,)
+                                          "e202303301540311", cookie_str)
     return ret_msg
