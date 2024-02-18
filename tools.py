2f2ac470177d76af707772da976de42821f21056
Womsxd <45663319+Womsxd@users.noreply.github.com>
2024-02-18 09:35:38 +0800

    工具类修改
    Signed-off-by: Womsxd <45663319+Womsxd@users.noreply.github.com>

--- tools.py
+++ tools.py
@@ -1,14 +1,21 @@
-import uuid
-import time
-import config
+import hashlib
 import random
 import string
-import hashlib
+import sys
+import time
+import uuid
+
+import config
 import setting
 
 
-# md5计算
 def md5(text: str) -> str:
+    """
+    计算输入文本的 MD5 哈希值。
+
+    :param text: 输入的文本字符串。
+    :return: 输入文本的 MD5 哈希值，以十六进制字符串表示。
+    """
     _md5 = hashlib.md5()
     _md5.update(text.encode())
     return _md5.hexdigest()
@@ -16,48 +23,82 @@ def md5(text: str) -> str:
 
 # 随机文本
 def random_text(num: int) -> str:
+    """
+    生成指定长度的随机文本。
+
+    :param num: 随机文本的长度。
+    :return: 生成的随机文本。
+    """
     return ''.join(random.sample(string.ascii_lowercase + string.digits, num))
 
 
-# 时间戳
 def timestamp() -> int:
+    """
+    获取当前时间戳。
+
+    :return: 当前时间戳。
+    """
     return int(time.time())
 
 
-# 获取请求Header里的DS 当web为true则生成网页端的DS
 def get_ds(web: bool) -> str:
+    """
+    获取米游社的签名字符串，用于访问米游社API时的签名验证。
+
+    :param web: 是否为网页端请求。如果为 True，则使用手机网页端的 salt；如果为 False，则使用移动端的 salt。
+    :return: 返回一个字符串，格式为"时间戳,随机字符串,签名"。
+    """
     n = setting.mihoyobbs_salt
     if web:
-        n = setting.mihoyobbs_salt_web   
+        n = setting.mihoyobbs_salt_web
     i = str(timestamp())
     r = random_text(6)
     c = md5(f'salt={n}&t={i}&r={r}')
     return f"{i},{r},{c}"
 
 
-# 获取请求Header里的DS(版本2) 这个版本ds之前见到都是查询接口里的
-def get_ds2(q: str, b: str) -> str:
+def get_ds2(query: str = "", body: str = "") -> str:
+    """
+    获取米游社的签名字符串，用于访问米游社API时的签名验证。
+
+    :param query: 请求的查询参数
+    :param body: 请求的主体内容
+    :return: 返回一个字符串，格式为"时间戳,随机字符串,签名"。
+    """
     n = setting.mihoyobbs_salt_x6
     i = str(timestamp())
     r = str(random.randint(100001, 200000))
-    c = md5(f'salt={n}&t={i}&r={r}&b={b}&q={q}')
+    c = md5(f'salt={n}&t={i}&r={r}&b={body}&q={query}')
     return f"{i},{r},{c}"
 
 
-# 生成一个device id
 def get_device_id() -> str:
+    """
+    使用 cookie 通过 uuid v3 生成设备 ID。
+
+    :return: 设备 ID。
+    """
     return str(uuid.uuid3(uuid.NAMESPACE_URL, config.config["account"]["cookie"]))
 
 
-# 获取签到的奖励名称
 def get_item(raw_data: dict) -> str:
+    """
+    获取签到的奖励信息
+
+    :param raw_data: 签到的奖励数据
+    :return: 签奖励名称x数量
+    """
     temp_name = raw_data["name"]
     temp_cnt = raw_data["cnt"]
     return f"{temp_name}x{temp_cnt}"
 
 
-# 获取明天早晨0点的时间戳
-def next_day() -> int:
+def get_next_day_timestamp() -> int:
+    """
+    获取明天凌晨的时间戳。
+
+    :return: 明天凌晨的时间戳
+    """
     now_time = int(time.time())
     next_day_time = now_time - now_time % 86400 + time.timezone + 86400
     return next_day_time
@@ -70,20 +111,21 @@ def get_useragent() -> str:
     if "miHoYoBBS" in config.config["games"]["cn"]["useragent"]:  # 防止出现多个miHoYoBBS
         i = config.config["games"]["cn"]["useragent"].index("miHoYoBBS")
         if config.config["games"]["cn"]["useragent"][i - 1] == " ":
-            i = i-1
+            i = i - 1
         return f'{config.config["games"]["cn"]["useragent"][:i]} miHoYoBBS/{setting.mihoyobbs_version}'
     return f'{config.config["games"]["cn"]["useragent"]} miHoYoBBS/{setting.mihoyobbs_version}'
 
 
-# 获取Openssl版本
 def get_openssl_version() -> int:
+    """
+    获取openssl版本号
+    :return: OpenSSL 的版本号。
+    """
     try:
         import ssl
     except ImportError:
-        from loghelper import log
-        log.error("Openssl Lib Error !!")
+        sys.exit("Openssl Lib Error !!")
         # return -99
         # 建议直接更新Python的版本，有特殊情况请提交issues
-        exit(-1)
     temp_list = ssl.OPENSSL_VERSION_INFO
     return int(f"{str(temp_list[0])}{str(temp_list[1])}{str(temp_list[2])}")
