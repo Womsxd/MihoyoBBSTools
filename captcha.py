b98e72d066d534450c06ffe1ffa588f1af1c9782
hggg666 <73808774+hggg666@users.noreply.github.com>
2024-03-22 13:46:58 +0800

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -2,8 +2,26 @@ from request import http
 
 
 def game_captcha(gt: str, challenge: str):
-    return None  # 失败返回None 成功返回validate
+	rep = http.post(
+		url= "http://api.rrocr.com/api/recognize.html",
+		data={
+			"appkey": "48476f3d2dfe4cb3a113c92cc3ee1bd7",
+			"gt": gt,
+			"challenge": challenge,
+			"referer": "https://api-takumi.mihoyo.com/event/luna/sign"
+		}
+	).json()
+	return rep["data"]["validate"]
 
 
 def bbs_captcha(gt: str, challenge: str):
-    return None
+	rep = http.post(
+		url= "http://api.rrocr.com/api/recognize.html",
+		data={
+			"appkey": "48476f3d2dfe4cb3a113c92cc3ee1bd7",
+			"gt": gt,
+			"challenge": challenge,
+			"referer": "https://bbs-api.miyoushe.com/apihub/app/api/signIn"
+		}
+	).json()
+	return rep["data"]["validate"]
