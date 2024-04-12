91764a59741a68e121065926721384edef1e4f56
Womsxd <45663319+Womsxd@users.noreply.github.com>
2024-04-12 08:39:42 +0800

    缩进处理
    Signed-off-by: Womsxd <45663319+Womsxd@users.noreply.github.com>

--- mihoyobbs.py
+++ mihoyobbs.py
@@ -37,16 +37,16 @@ class Mihoyobbs:
             "User-Agent": "okhttp/4.9.3"
         }
         self.task_do = {
-            "bbs_sign": False,
-            "bbs_read": False,
-            "bbs_read_num": 3,
-            "bbs_like": False,
-            "bbs_like_num": 5,
-            "bbs_share": False
+            "sign": False,
+            "read": False,
+            "read_num": 3,
+            "like": False,
+            "like_num": 5,
+            "share": False
         }
         self.get_tasks_list()
         # 如果这三个任务都做了就没必要获取帖子了
-        if self.task_do["bbs_read"] and self.task_do["bbs_like"] and self.task_do["bbs_share"]:
+        if self.task_do["read"] and self.task_do["like"] and self.task_do["share"]:
             pass
         else:
             self.postsList = self.get_list()
@@ -83,16 +83,16 @@ class Mihoyobbs:
         self.today_have_get_coins = data["data"]["already_received_points"]
         self.have_coins = data["data"]["total_points"]
         tasks = {
-            58: {"attr": "bbs_sign", "done": "is_get_award"},
-            59: {"attr": "bbs_read", "done": "is_get_award", "num_attr": "bbs_read_num"},
-            60: {"attr": "bbs_like", "done": "is_get_award", "num_attr": "bbs_like_num"},
-            61: {"attr": "bbs_share", "done": "is_get_award"}
+            58: {"attr": "sign", "done": "is_get_award"},
+            59: {"attr": "read", "done": "is_get_award", "num_attr": "read_num"},
+            60: {"attr": "like", "done": "is_get_award", "num_attr": "like_num"},
+            61: {"attr": "share", "done": "is_get_award"}
         }
         if self.today_get_coins == -1:
-            self.task_do.bbs_sign = True
-            self.task_do.bbs_read = True
-            self.task_do.bbs_like = True
-            self.task_do.bbs_share = True
+            self.task_do.sign = True
+            self.task_do.read = True
+            self.task_do.like = True
+            self.task_do.share = True
         else:
             for task in tasks.keys():
                 mission_state = next((x for x in data["data"]["states"] if x["mission_id"] == task), None)
@@ -128,58 +128,57 @@ class Mihoyobbs:
 
     # 进行签到操作
     def signing(self):
-        if self.task_do["bbs_sign"]:
+        if self.task_do["sign"]:
             log.info("讨论区任务已经完成过了~")
-        else:
-            log.info("正在签到......")
-            header = self.headers.copy()
-            for forum in self.bbs_list:
-                challenge = None
-                for retry_count in range(2):
-                    header["DS"] = tools.get_ds2("", json.dumps({"gids": forum["id"]}))
-                    req = http.post(url=setting.bbs_sign_url, json={"gids": forum["id"]}, headers=header)
-                    log.debug(req.text)
-                    data = req.json()
-                    if data["retcode"] == 1034:
-                        log.warning("社区签到触发验证码")
-                        challenge = self.get_pass_challenge()
-                        if challenge is not None:
-                            header["x-rpc-challenge"] = challenge
-                    elif "err" not in data["message"] and data["retcode"] == 0:
-                        log.info(str(forum["name"] + data["message"]))
-                        wait()
-                        break
-                    else:
-                        log.error("签到失败，你的cookie可能已过期，请重新设置cookie。")
-                        config.clear_cookies()
-                        raise CookieError('Cookie expires')
-                if challenge is not None:
-                    header.pop("x-rpc-challenge")
+            return
+        log.info("正在签到......")
+        header = self.headers.copy()
+        for forum in self.bbs_list:
+            challenge = None
+            for retry_count in range(2):
+                header["DS"] = tools.get_ds2("", json.dumps({"gids": forum["id"]}))
+                req = http.post(url=setting.bbs_sign_url, json={"gids": forum["id"]}, headers=header)
+                log.debug(req.text)
+                data = req.json()
+                if data["retcode"] == 1034:
+                    log.warning("社区签到触发验证码")
+                    challenge = self.get_pass_challenge()
+                    if challenge is not None:
+                        header["x-rpc-challenge"] = challenge
+                elif "err" not in data["message"] and data["retcode"] == 0:
+                    log.info(str(forum["name"] + data["message"]))
+                    wait()
+                    break
+                else:
+                    log.error("签到失败，你的cookie可能已过期，请重新设置cookie。")
+                    config.clear_cookies()
+                    raise CookieError('Cookie expires')
+            if challenge is not None:
+                header.pop("x-rpc-challenge")
 
     # 看帖子
     def read_posts(self):
-        if self.task_do["bbs_read"]:
+        if self.task_do["read"]:
             log.info("看帖任务已经完成过了~")
-        else:
-            log.info("正在看帖......")
-            for i in range(self.task_do["bbs_read_num"]):
-                req = http.get(url=setting.bbs_detail_url, params={"post_id": self.postsList[i][0]},
-                               headers=self.headers)
-                log.debug(req.text)
-                data = req.json()
-                if data["message"] == "OK":
-                    log.debug("看帖：{} 成功".format(self.postsList[i][1]))
-                wait()
+            return
+        log.info("正在看帖......")
+        for i in range(self.task_do["read_num"]):
+            req = http.get(url=setting.bbs_detail_url, params={"post_id": self.postsList[i][0]}, headers=self.headers)
+            log.debug(req.text)
+            data = req.json()
+            if data["message"] == "OK":
+                log.debug("看帖：{} 成功".format(self.postsList[i][1]))
+            wait()
 
     # 点赞
     def like_posts(self):
         header = self.headers.copy()
         challenge = None
-        if self.task_do["bbs_like"]:
+        if self.task_do["like"]:
             log.info("点赞任务已经完成过了~")
             return
         log.info("正在点赞......")
-        for i in range(self.task_do["bbs_like_num"]):
+        for i in range(self.task_do["like_num"]):
             req = http.post(url=setting.bbs_like_url, headers=header,
                             json={"post_id": self.postsList[i][0], "is_cancel": False})
             log.debug(req.text)
@@ -190,12 +189,14 @@ class Mihoyobbs:
                     challenge = None
                     header.pop("x-rpc-challenge")
                 # 判断取消点赞是否打开
-                if config.config["mihoyobbs"]["cancel_like"]:
+                if not config.config["mihoyobbs"]["cancel_like"]:
                     wait()
-                    req = http.post(url=setting.bbs_like_url, headers=self.headers,
-                                    json={"post_id": self.postsList[i][0], "is_cancel": True})
-                    if req.json()["message"] == "OK":
-                        log.debug("取消点赞：{} 成功".format(self.postsList[i][1]))
+                    continue
+                wait()
+                req = http.post(url=setting.bbs_like_url, headers=self.headers,
+                                json={"post_id": self.postsList[i][0], "is_cancel": True})
+                if req.json()["message"] == "OK":
+                    log.debug("取消点赞：{} 成功".format(self.postsList[i][1]))
             elif data["retcode"] == 1034:
                 log.warning("点赞触发验证码")
                 challenge = self.get_pass_challenge()
@@ -205,7 +206,7 @@ class Mihoyobbs:
 
     # 分享操作
     def share_post(self):
-        if self.task_do.get("bbs_share"):
+        if self.task_do.get("share"):
             log.info("分享任务已经完成过了~")
         else:
             log.info("正在执行分享任务......")
@@ -218,15 +219,14 @@ class Mihoyobbs:
                     log.debug(f"分享：{self.postsList[0][1]} 成功")
                     log.info("分享任务执行成功......")
                     break
-                else:
-                    log.debug(f"分享任务执行失败，正在执行第{i + 2}次，共3次")
-                    wait()
+                log.debug(f"分享任务执行失败，正在执行第{i + 2}次，共3次")
+                wait()
             wait()
 
     def run_task(self):
         return_data = "米游社: "
-        if self.task_do["bbs_sign"] and self.task_do["bbs_read"] and self.task_do["bbs_like"] and \
-                self.task_do["bbs_share"]:
+        if self.task_do["sign"] and self.task_do["read"] and self.task_do["like"] and \
+                self.task_do["share"]:
             return_data += "\n" + f"今天已经全部完成了！\n" \
                                   f"一共获得{self.today_have_get_coins}个米游币\n目前有{self.have_coins}个米游币"
             log.info(f"今天已经全部完成了！一共获得{self.today_have_get_coins}个米游币，目前有{self.have_coins}个米游币")
