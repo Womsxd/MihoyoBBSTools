b81095e301dfbf6d8b42ba3d6f29a09b2b1eac05
Womsxd <45663319+Womsxd@users.noreply.github.com>
2024-06-30 16:16:21 +0800

    misc:修改超时时长
    Signed-off-by: Womsxd <45663319+Womsxd@users.noreply.github.com>

--- request.py
+++ request.py
@@ -6,7 +6,7 @@ def get_new_session(**kwargs):
         # 优先使用httpx，在httpx无法使用的环境下使用requests
         import httpx
 
-        http_client = httpx.Client(timeout=20, transport=httpx.HTTPTransport(retries=10), follow_redirects=True,
+        http_client = httpx.Client(timeout=30, transport=httpx.HTTPTransport(retries=10), follow_redirects=True,
                                    **kwargs)
         # 当openssl版本小于1.0.2的时候直接进行一个空请求让httpx报错
         import tools
