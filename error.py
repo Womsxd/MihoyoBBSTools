125106238dbb58be6b1136c2b7e90ff62ede7089
Womsxd <45663319+Womsxd@users.noreply.github.com>
2024-08-09 09:34:21 +0800

    feat: 加入stoken的异常
    Signed-off-by: Womsxd <45663319+Womsxd@users.noreply.github.com>

--- error.py
+++ error.py
@@ -6,6 +6,14 @@ class CookieError(Exception):
         return repr(self.info)
 
 
+class StokenError(Exception):
+    def __init__(self, info):
+        self.info = info
+
+    def __str__(self):
+        return repr(self.info)
+
+
 class CaptchaError(Exception):
     def __init__(self, info):
         self.info = info
