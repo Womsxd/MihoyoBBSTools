ae4ebb10b42f6825043748a7b8e4bbb8af2593b0
hggg666 <73808774+hggg666@users.noreply.github.com>
2024-11-29 14:07:26 +0800

    我自己的自定义修改

--- captcha.py
+++ captcha.py
@@ -2,8 +2,30 @@ from request import http
 
 
 def game_captcha(gt: str, challenge: str):
-    return None  # 失败返回None 成功返回validate
+	appkey=config.config['captcha']['token']
+	log.warning(f"appkeygame：{appkey}")
+	rep = http.post(
+		url= "http://api.rrocr.com/api/recognize.html",
+		data={
+			"appkey": appkey,
+			"gt": gt,
+			"challenge": challenge,
+			"referer": "https://api-takumi.mihoyo.com/event/luna/sign"
+		}
+	).json()
+	return rep["data"]["validate"]
 
 
 def bbs_captcha(gt: str, challenge: str):
-    return None
+	appkey=config.config['captcha']['token']
+	log.warning(f"appkeybbs：{appkey}")
+	rep = http.post(
+		url= "http://api.rrocr.com/api/recognize.html",
+		data={
+			"appkey": appkey,
+			"gt": gt,
+			"challenge": challenge,
+			"referer": "https://bbs-api.miyoushe.com/apihub/app/api/signIn"
+		}
+	).json()
+	return rep["data"]["validate"]
