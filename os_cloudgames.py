cedbefb1a6762a3e7eb764923640dd74f7cd1bb3
CreeperAWA <CreeperAWA@creeper.us.kg>
2025-01-05 10:49:55 +0800

    修错别字

--- os_cloudgames.py
+++ os_cloudgames.py
@@ -29,8 +29,8 @@ class CloudGenshin:
                 log.info(f'签到成功，已获得{data["data"]["free_time"]["send_freetime"]}分钟免费时长')
                 ret_msg += f'签到成功，已获得{data["data"]["free_time"]["send_freetime"]}分钟免费时长\n'
             else:
-                log.info('签到失败，未获得免费时长，可能是已经签到过了或者超出免费时长上线')
-                ret_msg += '签到失败，未获得免费时长，可能是已经签到过了或者超出免费时长上线\n'
+                log.info('签到失败，未获得免费时长，可能是已经签到过了或者超出免费时长上限')
+                ret_msg += '签到失败，未获得免费时长，可能是已经签到过了或者超出免费时长上限\n'
             ret_msg += f'你当前拥有免费时长 {tools.time_conversion(int(data["data"]["free_time"]["free_time"]))} ,' \
                        f'畅玩卡状态为 {data["data"]["play_card"]["short_msg"]}，拥有米云币 {data["data"]["coin"]["coin_num"]} 枚'
             log.info(ret_msg)
