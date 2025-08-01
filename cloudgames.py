f32e98627a362cb428fc3459d376ad243f2cd041
owwkmidream <159dreaml@gmail.com>
2025-08-02 00:35:59 +0800

    fix(cloudgames): 修复云绝区零签到错误，修改云游戏基类
    - 修复CloudZZZ使用错误签到URL的问题
    - 基类支持传入url
    - 添加统一异常处理机制
    - 确保使用正确的cookie清除函数
    - 基类添加币种名参数

--- cloudgames.py
+++ cloudgames.py
@@ -9,51 +9,61 @@ from loghelper import log
 
 
 class CloudGameBase:
-    def __init__(self, game_name) -> None:
+    def __init__(self, game_name, sign_url, coin_name, clear_cookie_func) -> None:
         self.headers = {}
         self.game_name = game_name
+        self.sign_url = sign_url
+        self.coin_name = coin_name
+        self.clear_cookie_func = clear_cookie_func
 
     def sign_account(self) -> str:
+        log.info(f"{self.game_name}:")
         ret_msg = f"{self.game_name}:\r\n"
-        req = http.get(url=setting.cloud_genshin_sgin, headers=self.headers)
-        data = req.json()
+        
+        try:
+            req = http.get(url=self.sign_url, headers=self.headers)
+            data = req.json()
 
-        if data['retcode'] == 0:
-            free_time_data = data["data"]["free_time"]
-            free_time = int(free_time_data["free_time"])
-            send_free_time = int(free_time_data["send_freetime"])
+            if data['retcode'] == 0:
+                free_time_data = data["data"]["free_time"]
+                free_time = int(free_time_data["free_time"])
+                send_free_time = int(free_time_data["send_freetime"])
 
-            if send_free_time > 0:
-                log.info(f'签到成功，已获得 {send_free_time} 分钟免费时长')
-                ret_msg += f'签到成功，已获得 {send_free_time} 分钟免费时长\n'
+                if send_free_time > 0:
+                    log.info(f'签到成功，已获得 {send_free_time} 分钟免费时长')
+                    ret_msg += f'签到成功，已获得 {send_free_time} 分钟免费时长\n'
+                else:
+                    if free_time < 600:
+                        time.sleep(random.randint(3, 6))
+                        data2 = http.get(url=self.sign_url, headers=self.headers).json()
+                        free_time2 = int(data2["data"]["free_time"]["free_time"])
+                        if free_time2 > free_time:
+                            get_free_time = free_time2 - free_time
+                            log.info(f'签到成功，已获得 {get_free_time} 分钟免费时长')
+                            ret_msg += f'签到成功，已获得 {get_free_time} 分钟免费时长\n'
+                        else:
+                            log.info('签到失败，未获得免费时长，可能是已经签到过了或者超出免费时长上限')
+                            ret_msg += '签到失败，未获得免费时长，可能是已经签到过了或者超出免费时长上限\n'
+                ret_msg += f'你当前拥有免费时长 {tools.time_conversion(int(data["data"]["free_time"]["free_time"]))}，' \
+                           f'畅玩卡状态为 {data["data"]["play_card"]["short_msg"]}，拥有{self.coin_name} {data["data"]["coin"]["coin_num"]} 枚'
+                log.info(ret_msg)
+            elif data['retcode'] == -100:
+                ret_msg += f"token 失效/防沉迷"
+                log.warning(ret_msg)
+                self.clear_cookie_func()
             else:
-                if free_time < 600:
-                    time.sleep(random.randint(3, 6))
-                    data2 = http.get(url=setting.cloud_genshin_sgin, headers=self.headers).json()
-                    free_time2 = int(data2["data"]["free_time"]["free_time"])
-                    if free_time2 > free_time:
-                        get_free_time = free_time2 - free_time
-                        log.info(f'签到成功，已获得 {get_free_time} 分钟免费时长')
-                        ret_msg += f'签到成功，已获得 {get_free_time} 分钟免费时长\n'
-                    else:
-                        log.info('签到失败，未获得免费时长，可能是已经签到过了或者超出免费时长上限')
-                        ret_msg += '签到失败，未获得免费时长，可能是已经签到过了或者超出免费时长上限\n'
-            ret_msg += f'你当前拥有免费时长 {tools.time_conversion(int(data["data"]["free_time"]["free_time"]))}，' \
-                       f'畅玩卡状态为 {data["data"]["play_card"]["short_msg"]}，拥有米云币 {data["data"]["coin"]["coin_num"]} 枚'
-            log.info(ret_msg)
-        elif data['retcode'] == -100:
-            ret_msg = f"{self.game_name} token 失效/防沉迷"
-            log.warning(ret_msg)
-            config.clear_cookie_cloudgame_genshin()
-        else:
-            ret_msg = f'脚本签到失败，json 文本：{req.text}'
-            log.warning(ret_msg)
+                ret_msg += f'脚本签到失败，json 文本：{req.text}'
+                log.warning(ret_msg)
+        except Exception as e:
+            log.error(f'{self.game_name} 签到异常：{str(e)}')
+            ret_msg += '脚本签到发生异常，请查看日志'
+        
         return ret_msg
 
 
 class CloudGenshin(CloudGameBase):
     def __init__(self, token) -> None:
-        super().__init__("云原神")
+        super().__init__("云原神", setting.cloud_genshin_sgin, "米云币", config.clear_cookie_cloudgame_genshin)
         self.headers = {
             'Host': 'api-cloudgame.mihoyo.com',
             'Accept': '*/*',
@@ -67,7 +77,7 @@ class CloudGenshin(CloudGameBase):
 
 class CloudZZZ(CloudGameBase):
     def __init__(self, token) -> None:
-        super().__init__("云绝区零")
+        super().__init__("云绝区零", setting.cloud_zzz_sgin, "邦邦点", config.clear_cookie_cloudgame_zzz)
         self.headers = {
             'Host': 'cg-nap-api.mihoyo.com',
             'Accept': '*/*',
