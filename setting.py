72ed46a4fab84be2d1fc2c2c3c5027bdb0b8bcd2
Womsxd <45663319+Womsxd@users.noreply.github.com>
2025-08-29 13:13:53 +0800

    feat(setting): 更新Salt并添加新游戏支持

--- setting.py
+++ setting.py
@@ -1,14 +1,14 @@
 # 米游社的Salt
 # java提取，会跟随版本更新
-mihoyobbs_salt = "ss6ZUzUlaWv6iDe0SHPSdCZYr0RSKPdi"
-mihoyobbs_salt_web = "gW20AtTxpc0V5GR3SmsytCLhVBgXtW6I"
+mihoyobbs_salt = "LKsbRciURVKljyjuhSeobiyFngbAyjAh"
+mihoyobbs_salt_web = "dDIQHbKOdaPaLuvQKVzUzqdeCaxjtaPV"
 # so提取 一般不会变
 mihoyobbs_salt_x4 = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs"
 mihoyobbs_salt_x6 = "t0qEgfub6cvueAPgR5m9aQWWVciEer7v"
 # 部分接口要带
 mihoyobbs_verify_key = "bll8iq97cem8"
 # 米游社的版本
-mihoyobbs_version = "2.85.1"  # Salt和Version相互对应
+mihoyobbs_version = "2.90.1"  # Salt和Version相互对应
 # 米游社的客户端类型
 mihoyobbs_Client_type = "2"  # 1为ios 2为安卓
 mihoyobbs_Client_type_web = "5"  # 4为pc web 5为mobile web
@@ -21,7 +21,8 @@ mihoyobbs_List = {
     4: {"id": "4", "forumId": "37", "name": "未定事件簿"},
     5: {"id": "5", "forumId": "34", "name": "大别野"},
     6: {"id": "6", "forumId": "52", "name": "崩坏：星穹铁道"},
-    8: {"id": "8", "forumId": "57", "name": "绝区零"}
+    8: {"id": "8", "forumId": "57", "name": "绝区零"},
+    9: {"id": "9", "forumId": "948", "name": "崩坏：因缘精灵"},
 }
 
 game_id2name = {
@@ -30,7 +31,8 @@ game_id2name = {
     "nxx_cn": "未定事件簿",
     "hk4e_cn": "原神",
     "hkrpg_cn": "崩坏：星穹铁道",
-    "nap_cn": "绝区零"
+    "nap_cn": "绝区零",
+    "abc_cn": "崩坏：因缘精灵",
 }
 
 game_id2config = {
@@ -39,7 +41,8 @@ game_id2config = {
     "nxx_cn": "tears_of_themis",
     "hk4e_cn": "genshin",
     "hkrpg_cn": "honkaisr",
-    "nap_cn": "zzz"
+    "nap_cn": "zzz",
+    "abc_cn": "hna",
 }
 
 # 游戏签到的请求头
